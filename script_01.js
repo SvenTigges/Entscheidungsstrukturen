32b12585d20203a246ac112242ad597c0614f083
SvenTigges <sven.tigges@web.de>
2021-05-19 09:57:35 +0200

    control structures

--- script_01.js
+++ script_01.js
@@ -1,1 +1,20 @@
 // Entscheidungsstrukturen | control structures
+
+// Deklaration 
+let ageJohn, ageMark; 
+let isJohnOlder, isJohnEqual;
+
+// Wertzuweisung 
+ageJohn = 35;
+ageMark = 30;
+
+// Logische Aussagen / Tests
+isJohnOlder = (ageJohn > ageMark);
+isJohnEqual = (ageJohn == ageMark);
+
+// Ausgabe
+console.log("isJohnOlder: " + isJohnOlder);
+console.log("isJohnEqual: " + isJohnEqual);
+
+
+
