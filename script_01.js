fc48db0eb3bf14085f2fb2beecee5f481a037aa7
SvenTigges <sven.tigges@web.de>
2021-05-19 10:43:32 +0200

    IF

--- script_01.js
+++ script_01.js
@@ -13,8 +13,22 @@ isJohnOlder = (ageJohn > ageMark);
 isJohnEqual = (ageJohn == ageMark);
 
 // Ausgabe
-console.log("isJohnOlder: " + isJohnOlder);
-console.log("isJohnEqual: " + isJohnEqual);
+//console.log("isJohnOlder: " + isJohnOlder);
+//console.log("isJohnEqual: " + isJohnEqual);
 
+/*********** IF **********/
 
+//console.log("John ist älter"); // unbedingte Aussage 
 
+// alternativlos (TINA)
+
+// Bedingungs gebunden durch (if) Checked ob Ja oder Nein 
+/*
+//if (true) = Wahrheitswert           
+//if (isJohnOlder) = Test
+//if (ageJohn > ageMark) = Test
+if (35 > 30)
+{
+    console.log("John ist älter");  // codeblock = {}
+}
+*/
